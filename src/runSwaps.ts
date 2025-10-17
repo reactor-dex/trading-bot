2520f3457c83ae60c84f0aabccd32b0850ac8203
Andrey Koreshkov <koreshkov.andrey@gmail.com>
2025-10-17 14:16:21 +0300

    hotfix

--- src/runSwaps.ts
+++ src/runSwaps.ts
@@ -76,7 +76,7 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(3333, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3333');
 
     runSwaps();
 });
